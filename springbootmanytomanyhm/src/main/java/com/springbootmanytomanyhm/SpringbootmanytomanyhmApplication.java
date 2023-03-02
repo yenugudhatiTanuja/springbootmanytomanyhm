9a18797e3f95fb38f66ff485a0235ddd976fe571
yenugudhatiTanuja <107547575+yenugudhatiTanuja@users.noreply.github.com>
2023-03-02 15:59:47 +0530

    Add files via upload

---
+++ springbootmanytomanyhm/src/main/java/com/springbootmanytomanyhm/SpringbootmanytomanyhmApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmanytomanyhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmanytomanyhmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmanytomanyhmApplication.class, args);
+	}
+
+}
